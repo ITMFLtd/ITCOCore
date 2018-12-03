07256452d50eda70d987be8b1a6ced1d719c78b7
Zach <zachoooo@gmail.com>
2018-12-03 05:20:21 -0800

    Created basic configuration system

---
+++ src/main/java/com/itmf/core/ITCOCore.java
@@ -0,0 +1,45 @@
+package com.itmf.core;
+
+import com.itmf.core.utils.YamlConfiguration;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class ITCOCore {
+
+    public static Logger log;
+
+    public static void main(String[] args) {
+
+        log = Logger.getLogger("ITCOLog");
+        initLogger();
+
+        // Initialize configuration
+        YamlConfiguration config = new YamlConfiguration("config.yaml");
+
+        // Initialize block storage system
+
+        // Listen for UDP
+
+        // Broadcast keep alives
+
+    }
+
+    private static void initLogger() {
+
+        FileHandler fh;
+        try {
+            fh = new FileHandler("itco.log");
+            fh.setFormatter(new SimpleFormatter());
+            log.addHandler(fh);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        log.setUseParentHandlers(false);
+        log.info("Log initialized.");
+    }
+
+}
--- src/main/java/com/itmf/core/utils/YamlConfiguration.java
+++ src/main/java/com/itmf/core/utils/YamlConfiguration.java
@@ -1,4 +1,61 @@
 package com.itmf.core.utils;
 
+import com.itmf.core.ITCOCore;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class YamlConfiguration {
+
+    private Map<String, Object> config;
+    private String path;
+
+    public YamlConfiguration() {
+        this("config.yml");
+    }
+
+    public YamlConfiguration(String path) {
+        this.path = path;
+        ITCOCore.log.info("Initializing config " + path);
+        File file = new File(path);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        Yaml yaml = new Yaml();
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        config = yaml.load(inputStream);
+        if (config == null) {
+            config = new HashMap<String, Object>();
+        }
+    }
+
+    public Map<String, Object> getConfig() {
+        return config;
+    }
+
+    public void save() {
+        DumperOptions options = new DumperOptions();
+        options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+        Yaml yaml = new Yaml(options);
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        yaml.dump(config, writer);
+    }
+
 }
